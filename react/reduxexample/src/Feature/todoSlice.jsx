b684a88514abaed97e7c1051bb6a84f44928e321
urvashi <urvashi.d.bhavsar@gmail.com>
2025-03-19 12:22:08 +0530

    19th changes

--- react/reduxexample/src/Feature/todoSlice.jsx
+++ react/reduxexample/src/Feature/todoSlice.jsx
@@ -15,8 +15,18 @@ const todoSlice = createSlice({
             }
             state.todos.push(todo)
         },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter(todo => todo.id !== action.payload)
+        },
+        updateTodo: (state, action) => {
+            const { id, updateText } = action.payload
+            const todo = state.todos.find(todo => todo.id === id)
+            if (todo) {
+                todo.text = updateText;
+            }
+        },
     }
 })
 
-export const { addTodo } = todoSlice.actions;
+export const { addTodo, removeTodo, updateTodo } = todoSlice.actions;
 export default todoSlice.reducer;
--- react/reduxexample/src/Pages/Showtodo.jsx
+++ react/reduxexample/src/Pages/Showtodo.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { faPen, faTrash, faCheck } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { removeTodo, updateTodo } from '../Feature/todoSlice'
 
 const Showtodo = () => {
     const todo = useSelector(state => state.todos.todos)
+    const dispatch = useDispatch()
+    const [editid, setEditid] = useState(null)
+    const [edittext, setEditText] = useState("")
+
+    const removeData = (id) => {
+        dispatch(removeTodo(id))
+    }
+
+    const handleFetch = (id, text) => {
+        setEditid(id)
+        setEditText(text)
+    }
+
+    const handleUpdate = (id) => {
+        dispatch(updateTodo({ id, updateText: edittext }))
+        setEditid(null)
+        setEditText("")
+    }
 
     return (
         <>
@@ -11,13 +32,40 @@ const Showtodo = () => {
                     <thead className='bg-primary'>
                         <tr>
                             <th>Todo List Actions</th>
+                            <th className='text-end'>Action</th>
                         </tr>
                     </thead>
                     <tbody>
                         {
                             todo.map(items =>
                                 <tr key={items.id}>
-                                    <td>{items.text}</td>
+                                    <td>
+                                        {
+                                            editid === items.id ?
+                                                <input type="text" className='form-control' value={edittext} onChange={(e) => setEditText(e.target.value)} />
+                                                :
+                                                items.text
+                                        }
+                                    </td>
+
+                                    <td className='text-end'>
+                                        {
+                                            editid === items.id ?
+                                                <button className='btn btn-primary mx-1' onClick={() => handleUpdate(items.id)}>
+                                                    <FontAwesomeIcon icon={faCheck} />
+                                                </button>
+                                                :
+                                                <div>
+                                                    <button className='btn btn-primary mx-1' onClick={() => handleFetch(items.id, items.text)}>
+                                                        <FontAwesomeIcon icon={faPen} />
+                                                    </button>
+
+                                                    <button className='btn btn-danger mx-1' onClick={() => removeData(items.id)}>
+                                                        <FontAwesomeIcon icon={faTrash} />
+                                                    </button>
+                                                </div>
+                                        }
+                                    </td>
                                 </tr>
                             )
                         }
